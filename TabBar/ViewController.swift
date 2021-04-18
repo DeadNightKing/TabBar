b8615583bd4ced29b3f0600d7de3b35d499b0f30
Ryan Chou <king04174387@gmail.com>
2021-04-18 16:26:39 +0800

    second try

--- TabBar/ViewController.swift
+++ TabBar/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("hello page 1")
+        print("try")
+
     }
-
-
 }
 
